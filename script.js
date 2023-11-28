9eed203056eb2ed3344918635bdb2cb39cca5d2e
Kevin Vargas <TriGzYT@gmail.com>
2023-11-28 15:50:45 -0500

    Completed Part 2 Prime Time

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Part 1 Fizz Buzz
 for (let i = 1; i <= 100; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
         console.log("Fizz Buzz");
@@ -9,3 +10,27 @@ for (let i = 1; i <= 100; i++) {
         console.log(i);
     }
 }
+
+// Part 2 Prime Time
+
+let n = 4;
+
+while (true) {
+    let isPrime = true;
+
+    for (let i = 2; i < n; i++) {
+        if (n % i === 0) {
+            isPrime = false;
+            break;
+        }
+    }
+
+    if (isPrime) {
+        console.log(n);
+        break;
+    }
+
+    n++;
+}
+
+
